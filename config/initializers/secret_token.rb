3444c67634ecda974c7b15abbb29d298aa4bc001
Jack Booth <j.booth.07@aberdeen.ac.uk>
2013-05-07 15:57:26 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Winecellar::Application.config.secret_token = '0e6ed436d7e77d74b3ad086304f61475f49a3ba1130de71fa76bc289c6f6a377c6e8c0c4651f1aa883882a9e06c6032f8f43df0792e5c390d148f2fb516c7c0c'
